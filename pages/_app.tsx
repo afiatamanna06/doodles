57647f42d3f5f06fafc35e14672e473bfad77834
afiatamanna06 <afiatamanna06@gmail.com>
2022-11-07 11:43:27 +0600

    Update rainbowkit provider file structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,9 @@ import { Avatar, ChakraProvider, Image } from '@chakra-ui/react'
 import NavigationBar from '../components/navigation/NavigationBar'
 import Fonts from '../fonts/Fonts'
 import theme from '../styles/theme'
-import merge from 'lodash.merge'
-import { RainbowKitProvider, lightTheme, Theme, AvatarComponent } from '@rainbow-me/rainbowkit'
+import { RainbowKitProvider, AvatarComponent } from '@rainbow-me/rainbowkit'
 import { WagmiConfig } from 'wagmi'
-import { wagmiClient } from '../utils/RainbowKitProvider'
-import { chains } from '../utils/RainbowKitProvider'
+import { myCustomTheme, wagmiClient, chains } from '../utils/RainbowKitProvider'
 import { FaUserAlt } from 'react-icons/fa'
 
 const CustomAvatar: AvatarComponent = ({ address, ensImage, size }) => {
@@ -23,27 +21,6 @@ const CustomAvatar: AvatarComponent = ({ address, ensImage, size }) => {
     <Avatar bg="blackAlpha.500" icon={<FaUserAlt />} />
   );
 };
-
-const myCustomTheme: Theme = merge(lightTheme({
-  accentColor: '#5086DC',
-  accentColorForeground: 'white',
-  borderRadius: "large",
-  fontStack: 'system',
-  overlayBlur: 'small',
-}), {
-  colors: {
-    closeButton: "white",
-    modalBackground: "rgba(0, 0, 0, .5)",
-    modalText: "white",
-    modalTextSecondary: "rgba(255, 255, 255, 0.64)",
-    profileForeground: "rgba(0, 0, 0, .1)",
-    profileAction: "rgba(0, 0, 0, .4)",
-    profileActionHover: "rgba(140, 125, 234, .6)",
-  },
-  fonts: {
-    body: 'Chalkboard SE',
-  },
-} as Theme)
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- utils/RainbowKitProvider.ts
+++ utils/RainbowKitProvider.ts
@@ -1,8 +1,9 @@
 import '@rainbow-me/rainbowkit/styles.css';
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { getDefaultWallets, lightTheme, RainbowKitProvider, Theme } from '@rainbow-me/rainbowkit';
 import { chain, configureChains, createClient, WagmiConfig } from "wagmi"
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from "wagmi/providers/public"
+import merge from 'lodash.merge';
 
 export const { chains, provider }  = configureChains(
     [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
@@ -24,3 +25,25 @@ export const wagmiClient = createClient({
     connectors,
     provider
 })
+
+export const myCustomTheme: Theme = merge(lightTheme({
+    accentColor: '#5086DC',
+    accentColorForeground: 'white',
+    borderRadius: "large",
+    fontStack: 'system',
+    overlayBlur: 'small',
+  }), {
+    colors: {
+      closeButton: "white",
+      modalBackground: "rgba(0, 0, 0, .5)",
+      modalText: "white",
+      modalTextSecondary: "rgba(255, 255, 255, 0.64)",
+      profileForeground: "rgba(0, 0, 0, .1)",
+      profileAction: "rgba(0, 0, 0, .4)",
+      profileActionHover: "rgba(140, 125, 234, .6)",
+    },
+    fonts: {
+      body: 'Chalkboard SE',
+    },
+  } as Theme)
+
