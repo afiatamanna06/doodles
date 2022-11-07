3473aa5bf9673760a07bd45e743c023f6621de8e
afiatamanna06 <afiatamanna06@gmail.com>
2022-11-07 11:03:05 +0600

    Update rainbowkit connect wallet modal colors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,45 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
+import { Avatar, ChakraProvider, Image } from '@chakra-ui/react'
 import NavigationBar from '../components/navigation/NavigationBar'
 import Fonts from '../fonts/Fonts'
 import theme from '../styles/theme'
 import merge from 'lodash.merge'
-import { RainbowKitProvider, midnightTheme, Theme } from '@rainbow-me/rainbowkit'
+import { RainbowKitProvider, lightTheme, Theme, AvatarComponent } from '@rainbow-me/rainbowkit'
 import { WagmiConfig } from 'wagmi'
 import { wagmiClient } from '../utils/RainbowKitProvider'
 import { chains } from '../utils/RainbowKitProvider'
+import { FaUserAlt } from 'react-icons/fa'
 
-const myCustomTheme: Theme = merge(midnightTheme({
+const CustomAvatar: AvatarComponent = ({ address, ensImage, size }) => {
+  return ensImage ? (
+    <Image
+      src={ensImage}
+      width={size}
+      height={size}
+      alt=""
+    />
+  ) : (
+    <Avatar bg="blackAlpha.500" icon={<FaUserAlt />} />
+  );
+};
+
+const myCustomTheme: Theme = merge(lightTheme({
   accentColor: '#5086DC',
   accentColorForeground: 'white',
   borderRadius: "large",
   fontStack: 'system',
   overlayBlur: 'small',
 }), {
+  colors: {
+    closeButton: "white",
+    modalBackground: "rgba(0, 0, 0, .5)",
+    modalText: "white",
+    modalTextSecondary: "rgba(255, 255, 255, 0.64)",
+    profileForeground: "rgba(0, 0, 0, .1)",
+    profileAction: "rgba(0, 0, 0, .4)",
+    profileActionHover: "rgba(140, 125, 234, .6)",
+  },
   fonts: {
     body: 'Chalkboard SE',
   },
@@ -27,6 +50,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <ChakraProvider theme={theme}>
       <WagmiConfig client={wagmiClient}>
         <RainbowKitProvider 
+          avatar={CustomAvatar}
           theme={myCustomTheme} 
           chains={chains}>
           <Fonts />
