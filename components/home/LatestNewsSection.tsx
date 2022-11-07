dfd89ec983b1e50c23de4656ae59cbe505063eed
afiatamanna06 <afiatamanna06@gmail.com>
2022-11-07 18:52:24 +0600

    Update latest news section styles

--- components/home/LatestNewsSection.tsx
+++ components/home/LatestNewsSection.tsx
@@ -5,10 +5,10 @@ function LatestNewsSection() {
   return (
     <Box w="full" bg="#80B1FF" px="4" py="6">
       <Center>
-        <Flex maxW="74rem" color="white" w="full" justify="space-between" alignItems="center">
+        <Flex maxW="74rem" gap={5} direction={["column", "column", "column", "row", "row"]} color="white" w="full" justify="space-between" alignItems={["start", "start", "start", "center", "center"]}>
             <Flex direction="column">
-                <Box textShadow="0px 2px 0px #5086dc">Doodles2 is coming soon</Box>
-                <Box>get the latest news delivered to your inbox</Box>
+                <Box textShadow="0px 2px 0px #5086dc" fontSize="2xl">Doodles2 is coming soon</Box>
+                <Box fontSize="lg">get the latest news delivered to your inbox</Box>
             </Flex>
             <SignUpInput />
         </Flex>
--- components/home/SignUpInput.tsx
+++ components/home/SignUpInput.tsx
@@ -2,8 +2,8 @@ import { Button, Flex, Input } from '@chakra-ui/react'
 
 function SignUpInput() {
   return (
-    <Flex gap={3} alignItems="center">
-        <Input color="black" borderRadius="xl" size="sm" />
+    <Flex gap={[5, 5, 5, 3, 3]} direction={["column", "column", "column", "row", "row"]} alignItems={["start", "start", "start", "center", "center"]}>
+        <Input color="black" type="email" bg="white" border="1px" borderColor="#D1B6B6" placeholder="Enter you email" _placeholder={{ color: "#D1B6B6" }} borderRadius="xl" size="sm" />
         <Button borderBottom="2px" bg="white" px={8} h="2rem" color="#6681c5" borderRadius="lg" borderColor="#6681c5">sign up</Button>
     </Flex>
   )
