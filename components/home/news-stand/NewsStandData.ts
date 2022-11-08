b392bc8094145ab53edf475964f04a7a82a5669a
afiatamanna06 <afiatamanna06@gmail.com>
2022-11-08 20:06:18 +0600

    Add news stand section data

--- components/home/news-stand/NewsStandData.ts
+++ components/home/news-stand/NewsStandData.ts
@@ -16,15 +16,15 @@ export const newsStandData = [
     {
         path: "https://markets.businessinsider.com/news/currencies/doodles-nfts-value-recession-ceo-julian-holguin-nftnyc-crypto-markets-2022-6",
         image: "/popup.png",
-        title: "Reddit's Ohanian Leads $54 Million Doodles Capital Raise",
-        source: "Forbes",
-        date: "Sep 13, 2022",
+        title: "Doodles NFTs hit $500 million in sales and could still weather recession",
+        source: "Business Insider",
+        date: "Jun 23, 2022",
     },
     {
-        path: "https://www.forbes.com/sites/mariagraciasantillanalinares/2022/09/13/reddits-ohanian-leads-54-million-doodles-capital-raise",
-        image: "/canon.png",
-        title: "Reddit's Ohanian Leads $54 Million Doodles Capital Raise",
-        source: "Forbes",
-        date: "Sep 13, 2022",
+        path: "https://www.billboard.com/pro/pharrell-williams-doodles-chief-brand-officer-board-member/",
+        image: "/pharrell.png",
+        title: "Pharrell Williams Joins Doodles as Chief Brand Officer & Board Member",
+        source: "Billboard",
+        date: "Jun 22, 2022",
     },
 ]
