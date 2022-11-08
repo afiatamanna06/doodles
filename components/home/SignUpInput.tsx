24bdef1673aa99dd87b2cfa34848992cec6040ec
afiatamanna06 <afiatamanna06@gmail.com>
2022-11-08 22:50:19 +0600

    Update news stand section style

--- components/home/SignUpInput.tsx
+++ components/home/SignUpInput.tsx
@@ -3,7 +3,7 @@ import { Button, Flex, Input } from '@chakra-ui/react'
 function SignUpInput() {
   return (
     <Flex gap={[5, 5, 5, 3, 3]} direction={["column", "column", "column", "row", "row"]} alignItems={["start", "start", "start", "center", "center"]}>
-        <Input color="black" type="email" bg="white" border="1px" borderColor="#D1B6B6" placeholder="Enter you email" _placeholder={{ color: "#D1B6B6" }} borderRadius="xl" size="sm" />
+        <Input minW={["17rem", "17rem", "22rem", "14rem", "14rem"]} color="black" type="email" bg="white" border="1px" borderColor="#D1B6B6" placeholder="Enter you email" _placeholder={{ color: "#D1B6B6" }} borderRadius="xl" size="sm" />
         <Button borderBottom="2px" bg="white" px={8} h="2rem" color="#6681c5" borderRadius="lg" borderColor="#6681c5">sign up</Button>
     </Flex>
   )
--- components/home/news-stand/NewsStandSection.tsx
+++ components/home/news-stand/NewsStandSection.tsx
@@ -1,4 +1,5 @@
 import { Box, Center, Flex, Grid, Image } from '@chakra-ui/react'
+import SignUpInput from '../SignUpInput'
 import NewsStandCard from './NewsStandCard'
 import { newsStandData } from './NewsStandData'
 
@@ -8,11 +9,16 @@ function NewsStandSection() {
       <Center>
         <Flex maxW="74rem" gap={5} direction={["column"]} color="white" w="full" alignItems={["center"]}>
             <Image src="/the-news-stand.png" w={["80%", "80%", "40%", "30%", "25%"]} h={["80%", "80%", "40%", "30%", "25%"]} alt="" />
-            <Grid gap={4} mt="2" templateColumns={["repeat(1, minmax(18rem, 1fr))", "repeat(1, minmax(18rem, 1fr))", "repeat(2, minmax(18rem, 1fr))", "repeat(3, minmax(18rem, 1fr))", "repeat(4, minmax(18rem, 1fr))"]}>
+            <Grid gap={4} mt="3" templateColumns={["repeat(1, minmax(18rem, 1fr))", "repeat(1, minmax(18rem, 1fr))", "repeat(2, minmax(18rem, 1fr))", "repeat(3, minmax(18rem, 1fr))", "repeat(4, minmax(18rem, 1fr))"]}>
                 {newsStandData.map(({ path, image, title, date, source}) => (
                     <NewsStandCard key={title} path={path} image={image} date={date} title={title} source={source} />
                 ))}
             </Grid>
+            <Flex direction={["column", "column", "column", "row", "row"]} gap={[0, 0, 0, 2]} textShadow="0px 2px 0px #80b1ff" fontSize="2xl" textAlign="center">
+                <Box>Get Doodles news</Box>
+                <Box> delivered to your inbox</Box>
+            </Flex>
+            <SignUpInput />
         </Flex>
       </Center>
     </Box>
