5f285473baeb4a7c4a9fccbed5f06c9373b36149
afiatamanna06 <afiatamanna06@gmail.com>
2022-11-08 21:58:09 +0600

    Add news stand data mapping

--- components/home/news-stand/NewsStandCard.tsx
+++ components/home/news-stand/NewsStandCard.tsx
@@ -13,17 +13,17 @@ type propTypes = {
 function NewsStandCard({ path, image, title, source, date }: propTypes) {
   return (
     <Link href={path} passHref>
-        <ChakraLink display="flex" flexDir="column" rounded="lg">
-            <Image src={image} w="15rem" h="10rem" alt="" />
-            <Flex direction="column" justify="space-between">
-                <Box>{title}</Box>
-                <Flex>
-                    <Box>{source}</Box>
-                    <FaCircle />
-                    <Box>{date}</Box>
+            <Flex direction="column" color="#313A71" _hover={{ textDecoration: "underline" }} rounded="lg" bg="white" h="100%">
+                <Image src={image} w="100%" borderTopRadius="lg" minH="9rem" objectFit="cover" alt="" />
+                <Flex p="4" color="#AD9999" h="100%" direction="column" justify="space-between">
+                    <Box color="#313A71" fontWeight={600}>{title}</Box>
+                    <Flex fontSize="sm" gap={2} alignItems="center">
+                        <Box fontWeight="semibold">{source}</Box>
+                        <FaCircle size={5} />
+                        <Box fontWeight="thin">{date}</Box>
+                    </Flex>
                 </Flex>
             </Flex>
-        </ChakraLink>
     </Link>
   )
 }
--- components/home/news-stand/NewsStandSection.tsx
+++ components/home/news-stand/NewsStandSection.tsx
@@ -1,4 +1,6 @@
-import { Box, Center, Flex, Image } from '@chakra-ui/react'
+import { Box, Center, Flex, Grid, Image } from '@chakra-ui/react'
+import NewsStandCard from './NewsStandCard'
+import { newsStandData } from './NewsStandData'
 
 function NewsStandSection() {
   return (
@@ -6,6 +8,11 @@ function NewsStandSection() {
       <Center>
         <Flex maxW="74rem" gap={5} direction={["column"]} color="white" w="full" alignItems={["center"]}>
             <Image src="/the-news-stand.png" w={["80%", "80%", "40%", "30%", "25%"]} h={["80%", "80%", "40%", "30%", "25%"]} alt="" />
+            <Grid gap={4} mt="2" templateColumns={["repeat(1, minmax(18rem, 1fr))", "repeat(1, minmax(18rem, 1fr))", "repeat(2, minmax(18rem, 1fr))", "repeat(3, minmax(18rem, 1fr))", "repeat(4, minmax(18rem, 1fr))"]}>
+                {newsStandData.map(({ path, image, title, date, source}) => (
+                    <NewsStandCard key={title} path={path} image={image} date={date} title={title} source={source} />
+                ))}
+            </Grid>
         </Flex>
       </Center>
     </Box>
