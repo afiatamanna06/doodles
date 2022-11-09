a88b14a9f6945a22af50ee74cd30197839b950c3
afiatamanna06 <afiatamanna06@gmail.com>
2022-11-10 00:18:27 +0600

    Add footer data

---
+++ components/footer/FooterLinks.tsx
@@ -0,0 +1,79 @@
+import { FaTwitter, FaYoutube, FaInstagram } from "react-icons/fa"
+
+export const footerLinks = [
+    {
+        path: "/",
+        name: "Browse",
+    },
+    {
+        path: "/",
+        name: "Dooplicator",
+    },
+    {
+        path: "/",
+        name: "About",
+    },
+    {
+        path: "/",
+        name: "Docs",
+    },
+    {
+        path: "/",
+        name: "Space Port",
+    },
+    {
+        path: "/",
+        name: "Launch",
+    },
+    {
+        path: "/",
+        name: "Docs",
+    },
+    {
+        path: "/",
+        name: "Genesis Box",
+    },
+    {
+        path: "/",
+        name: "Watch Trailer",
+    },
+    {
+        path: "/",
+        name: "DoodlePutt",
+    },
+    {
+        path: "/",
+        name: "NFT NYC 2022",
+    },
+    {
+        path: "/",
+        name: "SXSW 2022",
+    },
+    {
+        path: "/",
+        name: "Careers",
+    },
+]
+
+export const socialLinks = [
+    {
+        path: "/",
+        icon: <FaTwitter />,
+    },
+    {
+        path: "/",
+        icon: <FaInstagram />,
+    },
+    {
+        path: "/",
+        icon: <FaYoutube />,
+    },
+]
+
+
+
+
+
+
+
+
