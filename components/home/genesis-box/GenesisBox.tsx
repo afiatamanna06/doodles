98de756b875282224354c8e86ab4417bbab9f741
afiatamanna06 <afiatamanna06@gmail.com>
2022-11-12 13:04:39 +0600

    Update genesis box file structure

--- components/home/genesis-box/GenesisBox.tsx
+++ components/home/genesis-box/GenesisBox.tsx
@@ -1,5 +1,6 @@
 import { Box, Button, Flex, Image } from "@chakra-ui/react";
 import GenesisBoxMobile from "./GenesisBoxMobile";
+import GenesisBoxWeb from "./GenesisBoxWeb";
 
 function GenesisBox() {
   return (
@@ -18,60 +19,7 @@ function GenesisBox() {
           alignItems={["center"]}
         >
           <GenesisBoxMobile />
-          <Flex
-            display={["none", "none", "none", "flex", "flex"]}
-            maxW="74rem"
-            color="white"
-            bgImage="url('/genbox.png')"
-            bgSize={["cover"]}
-            bgPosition="right"
-            borderRadius="2xl"
-            bgRepeat="no-repeat"
-            w="full"
-            alignItems={["start", "start", "start", "center", "center"]}
-          >
-            <Flex
-              direction={["column", "column", "column", "row", "row"]}
-              gap={[8, 8, 8, 20, 20]}
-              w="100%"
-              maxH={["100%", "100%", "100%", "27rem"]}
-              p="14"
-            >
-              <Flex direction="column" gap="8">
-                <Image src="/text_genbox-promo.png" w="70%" alt="" />
-                <Box maxW={["35rem"]} fontSize="xl" fontWeight="medium">
-                  Genesis boxes contain a rare assortment of apparel and
-                  accessories you can use on day one of Doodles 2.
-                </Box>
-                <Flex
-                  direction="column"
-                  gap={3}
-                  w="min-content"
-                  alignItems="center"
-                >
-                  <Button
-                    borderBottomWidth="2px"
-                    color="white"
-                    borderColor="#6681c5"
-                    px={8}
-                    bg="#80B1FF"
-                    py={6}
-                    borderRadius="xl"
-                  >
-                    <Box fontSize="lg">buy on opensea</Box>
-                  </Button>
-                  <Box
-                    textShadow="0px 2px #2e2731"
-                    fontWeight="medium"
-                    fontSize="lg"
-                  >
-                    learn more
-                  </Box>
-                </Flex>
-              </Flex>
-              <Image src="/genbox.gif" w="25rem" h="100%" alt="" />
-            </Flex>
-          </Flex>
+          <GenesisBoxWeb />
         </Flex>
       </Flex>
     </Box>
--- components/home/genesis-box/GenesisBoxMobile.tsx
+++ components/home/genesis-box/GenesisBoxMobile.tsx
@@ -1,5 +1,4 @@
 import { Box, Button, Flex, Image } from "@chakra-ui/react";
-import React from "react";
 
 function GenesisBoxMobile() {
   return (
