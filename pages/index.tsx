11f727093a0540e6c50452ef48a5c28986a63204
afiatamanna06 <afiatamanna06@gmail.com>
2022-11-12 12:18:47 +0600

    Add genesis box section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@chakra-ui/react'
 import Head from 'next/head'
 import AboutSection from '../components/home/about/AboutSection'
+import GenesisBox from '../components/home/genesis-box/GenesisBox'
 import HomeSection from '../components/home/HomeSection'
 import LatestNewsSection from '../components/home/latest-news/LatestNewsSection'
 import NewsStandSection from '../components/home/news-stand/NewsStandSection'
@@ -16,6 +17,7 @@ export default function Home() {
       <HomeSection />
       <LatestNewsSection />
       <AboutSection />
+      <GenesisBox />
       <NewsStandSection />
     </div>
   )
