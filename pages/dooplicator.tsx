6467e25b0b4f27f035adf8b93e68f56cbaf3e4b6
afiatamanna06 <afiatamanna06@gmail.com>
2022-11-14 12:24:03 +0600

    Add dooplicator page description

--- pages/dooplicator.tsx
+++ pages/dooplicator.tsx
@@ -1,4 +1,4 @@
-import { Flex, Image } from "@chakra-ui/react"
+import { Box, Flex, Image } from "@chakra-ui/react"
 
 function Dooplicator() {
   return (
@@ -11,7 +11,15 @@ function Dooplicator() {
           w="full"
           alignItems={["start", "start", "center", "center", "center"]}
         >
-            <Image src="/dooplicator.gif" alt="" />
+            <Image src="/dooplicator.gif" w={["100%", "100%", "70%", "50%", "50%"]} alt="" />
+            <Flex direction="column">
+              <Box maxW={["35rem"]} color="#8B7EEA" fontSize="xl" fontWeight="medium">
+                An ordinary device that comes with extraordinary powers usable in Doodles2. You can catch a peak of the Dooplicator in action in the Doodles 2 Trailer revealed on June 21, 2022 at NFT NYC.
+              </Box>
+              <Box mt="6" maxW={["35rem"]} color="#8B7EEA" fontSize="xl" fontWeight="medium">
+                What&apos;s inside? Well, all we can say for now is that it’s one functional piece of Doodle machinery containing nebulous Doodley matter.
+              </Box>
+            </Flex>
         </Flex>
     </Flex>
   )
