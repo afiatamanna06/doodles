9c0c9c24391b5aa0b7ef1e676f12c8fc9e5a5be4
afiatamanna06 <afiatamanna06@gmail.com>
2022-11-14 09:27:03 +0600

    Update web version navigation bar structure

--- components/navigation/NavigationBar.tsx
+++ components/navigation/NavigationBar.tsx
@@ -21,7 +21,9 @@ import React from "react";
 import { FiChevronDown, FiMenu } from "react-icons/fi";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { IoClose } from "react-icons/io5";
+import Link from "next/link";
 import NavigationBarDrawer from "./NavigationBarDrawer";
+import { navigationLinks } from "./NavigationLinks";
 
 function NavigationBar() {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -62,95 +64,73 @@ function NavigationBar() {
             justify="center"
             w="full"
           >
+            {navigationLinks.slice(0, 3).map(({ title, links }) => (
+              <Flex key={title} flex="1" justify="center">
+                <Menu size="sm">
+                  <MenuButton
+                    as={Button}
+                    variant="ghost"
+                    fontSize="xl"
+                    rightIcon={<FiChevronDown />}
+                  >
+                    {title}
+                  </MenuButton>
+                  <MenuList
+                    color="rgb(140, 125, 234)"
+                    boxShadow="dark-lg"
+                    borderRadius="xl"
+                  >
+                    {links.map(({ path, name, target }) => (
+                      <Link
+                        key={path}
+                        href={path}
+                        target={target ? "_blank" : "_self"}
+                        passHref
+                      >
+                        <MenuItem>{name}</MenuItem>
+                      </Link>
+                    ))}
+                  </MenuList>
+                </Menu>
+              </Flex>
+            ))}
             <Flex flex="1" justify="center">
-              <Menu size="sm">
-                <MenuButton
-                  as={Button}
-                  variant="ghost"
-                  fontSize="xl"
-                  rightIcon={<FiChevronDown />}
-                >
-                  Doodles
-                </MenuButton>
-                <MenuList
-                  color="rgb(140, 125, 234)"
-                  boxShadow="dark-lg"
-                  borderRadius="xl"
-                >
-                  <MenuItem>Browse</MenuItem>
-                  <MenuItem>Dooplicator</MenuItem>
-                  <MenuItem>Doodlebank</MenuItem>
-                  <MenuItem>About</MenuItem>
-                  <MenuItem>Docs</MenuItem>
-                </MenuList>
-              </Menu>
+              <Link href="/" passHref>
+                <Button variant="ghost" fontSize="xl">
+                  Shop
+                </Button>
+              </Link>
             </Flex>
-            <Flex flex="1" justify="center">
-              <Menu size="sm">
-                <MenuButton
-                  as={Button}
-                  variant="ghost"
-                  fontSize="xl"
-                  rightIcon={<FiChevronDown />}
-                >
-                  Space Doodles
-                </MenuButton>
-                <MenuList
-                  color="rgb(140, 125, 234)"
-                  boxShadow="dark-lg"
-                  borderRadius="xl"
-                >
-                  <MenuItem>Space Port</MenuItem>
-                  <MenuItem>Launch</MenuItem>
-                  <MenuItem>About</MenuItem>
-                </MenuList>
-              </Menu>
-            </Flex>
-            <Flex flex="1" justify="center">
-              <Menu size="sm">
-                <MenuButton
-                  as={Button}
-                  variant="ghost"
-                  fontSize="xl"
-                  rightIcon={<FiChevronDown />}
-                >
-                  Doodles2
-                </MenuButton>
-                <MenuList
-                  color="rgb(140, 125, 234)"
-                  boxShadow="dark-lg"
-                  borderRadius="xl"
-                >
-                  <MenuItem>Genesis Box</MenuItem>
-                  <MenuItem>Watch Trailer</MenuItem>
-                </MenuList>
-              </Menu>
-            </Flex>
-            <Flex flex="1" justify="center">
-              <Button variant="ghost" fontSize="xl">
-                Shop
-              </Button>
-            </Flex>
-            <Flex flex="1" justify="center">
-              <Menu size="sm">
-                <MenuButton
-                  as={Button}
-                  variant="ghost"
-                  fontSize="xl"
-                  rightIcon={<FiChevronDown />}
-                >
-                  Events
-                </MenuButton>
-                <MenuList
-                  color="rgb(140, 125, 234)"
-                  boxShadow="dark-lg"
-                  borderRadius="xl"
-                >
-                  <MenuItem>SXSW 2022</MenuItem>
-                  <MenuItem>NFT NYC 2022</MenuItem>
-                </MenuList>
-              </Menu>
-            </Flex>
+            {navigationLinks.slice(3, 5).map(({ title, links }) => (
+              <Flex key={title} flex="1" justify="center">
+                <Menu size="sm">
+                  <MenuButton
+                    as={Button}
+                    variant="ghost"
+                    fontSize="xl"
+                    rightIcon={<FiChevronDown />}
+                  >
+                    {title}
+                  </MenuButton>
+                  <MenuList
+                    color="rgb(140, 125, 234)"
+                    boxShadow="dark-lg"
+                    borderRadius="xl"
+                  >
+                    {links.map(({ path, name, target }) => (
+                      <Link
+                        key={path}
+                        href={path}
+                        target={target ? "_blank" : "_self"}
+                        passHref
+                      >
+                        <MenuItem>{name}</MenuItem>
+                      </Link>
+                    ))}
+                  </MenuList>
+                </Menu>
+              </Flex>
+            ))}
           </Flex>
           <Button
             as={ConnectButton}
