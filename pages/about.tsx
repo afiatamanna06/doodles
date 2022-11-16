1e78be60bed8feaf975667603848c3917cd9c3ab
afiatamanna06 <afiatamanna06@gmail.com>
2022-11-16 20:20:01 +0600

    Add about page

---
+++ pages/about.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable react/jsx-key */
+import { Flex, Image } from "@chakra-ui/react";
+import { motion } from "framer-motion";
+import Marquee from "react-fast-marquee";
+
+const marqueeVariants = {
+  animate: {
+    x: ["100vw", "-100vw"],
+    transition: {
+      x: {
+        repeat: Infinity,
+        repeatType: "loop",
+        duration: 5,
+        ease: "linear",
+      },
+    },
+  },
+};
+
+function About() {
+  return (
+    <Flex direction="column" w="full">
+      <Marquee
+        delay={5}
+        direction="right"
+        play={true}
+        gradientWidth={50}
+        pauseOnHover={true}
+      >
+        <Image src="/about1.png" w="100%" height="10rem" alt="" />
+        <Image src="/about2.png" w="100%" height="10rem" alt="" />
+        <Image src="/about3.png" w="100%" height="10rem" alt="" />
+        <Image src="/about4.png" w="100%" height="10rem" alt="" />
+        <Image src="/about5.png" w="100%" height="10rem" alt="" />
+        <Image src="/about6.png" w="100%" height="10rem" alt="" />
+        <Image src="/about7.png" w="100%" height="10rem" alt="" />
+        <Image src="/about8.png" w="100%" height="10rem" alt="" />
+        <Image src="/about9.png" w="100%" height="10rem" alt="" />
+        <Image src="/about10.png" w="100%" height="10rem" alt="" />
+      </Marquee>
+    </Flex>
+  );
+}
+
+export default About;
