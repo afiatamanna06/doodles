83aa4cd63f329d1db78f0134bc1a6a53f985940f
afiatamanna06 <afiatamanna06@gmail.com>
2022-11-18 19:09:42 +0600

    Update about section parallax images links

--- components/about/AboutDescription.tsx
+++ components/about/AboutDescription.tsx
@@ -1,6 +1,7 @@
 import { Box, Button, Center, Flex, Image } from "@chakra-ui/react";
 import Link from "next/link";
 import { Parallax } from "react-scroll-parallax";
+import { aboutDescriptionImageData } from "./AboutDescriptionImage";
 
 function AboutDescription() {
   return (
@@ -64,38 +65,42 @@ function AboutDescription() {
           </Flex>
           <Flex gap={8} display={["none", "none", "none", "flex", "flex"]}>
             <Flex direction="column">
-              <Parallax speed={14}>
-                <Image src="/doodle-card-mascot.png" w={["100%"]} alt="" />
-              </Parallax>
-              <Parallax speed={14}>
-                <Image src="/doodle-card-rare.png" w={["100%"]} alt="" />
-              </Parallax>
+              {aboutDescriptionImageData.slice(0, 2).map(({ image, path }) => (
+                <Parallax speed={14} key={path}>
+                  <Link href={path} target="_blank" passHref>
+                    <Image src={image} w={["100%"]} alt="" />
+                  </Link>
+                </Parallax>
+              ))}
             </Flex>
             <Flex direction="column" gap={6} mt="20">
-              <Parallax speed={8}>
-                <Image src="/doodle-card-alien.png" w={["100%"]} alt="" />
-              </Parallax>
-              <Parallax speed={8}>
-                <Image src="/doodle-card-common.png" w={["100%"]} alt="" />
-              </Parallax>
+              {aboutDescriptionImageData.slice(2, 4).map(({ image, path }) => (
+                <Parallax speed={8} key={path}>
+                  <Link href={path} target="_blank" passHref>
+                    <Image src={image} w={["100%"]} alt="" />
+                  </Link>
+                </Parallax>
+              ))}
             </Flex>
           </Flex>
           <Flex gap={8} display={["flex", "flex", "flex", "none", "none"]}>
             <Flex direction="column" gap={5}>
-              <Parallax speed={3}>
-                <Image src="/doodle-card-mascot.png" w={["100%"]} alt="" />
-              </Parallax>
-              <Parallax speed={3}>
-                <Image src="/doodle-card-rare.png" w={["100%"]} alt="" />
-              </Parallax>
+              {aboutDescriptionImageData.slice(0, 2).map(({ image, path }) => (
+                <Parallax speed={3} key={path}>
+                  <Link href={path} target="_blank" passHref>
+                    <Image src={image} w={["100%"]} alt="" />
+                  </Link>
+                </Parallax>
+              ))}
             </Flex>
             <Flex direction="column" gap={6} mt="20">
-              <Parallax speed={2}>
-                <Image src="/doodle-card-alien.png" w={["100%"]} alt="" />
-              </Parallax>
-              <Parallax speed={2}>
-                <Image src="/doodle-card-common.png" w={["100%"]} alt="" />
-              </Parallax>
+              {aboutDescriptionImageData.slice(2, 4).map(({ image, path }) => (
+                <Parallax speed={2} key={path}>
+                  <Link href={path} target="_blank" passHref>
+                    <Image src={image} w={["100%"]} alt="" />
+                  </Link>
+                </Parallax>
+              ))}
             </Flex>
           </Flex>
         </Flex>
---
+++ components/about/AboutDescriptionImage.ts
@@ -0,0 +1,18 @@
+export const aboutDescriptionImageData = [
+    {
+        image: "/doodle-card-mascot.png", 
+        path: "https://opensea.io/assets/ethereum/0x8a90cab2b38dba80c64b7734e58ee1db38b8992e/316"
+    },
+    {
+        image: "/doodle-card-rare.png", 
+        path: "https://opensea.io/assets/ethereum/0x8a90cab2b38dba80c64b7734e58ee1db38b8992e/8293"
+    },
+    {
+        image: "/doodle-card-alien.png", 
+        path: "https://opensea.io/assets/ethereum/0x8a90cab2b38dba80c64b7734e58ee1db38b8992e/2912"
+    },
+    {
+        image: "/doodle-card-common.png", 
+        path: "https://opensea.io/assets/ethereum/0x8a90cab2b38dba80c64b7734e58ee1db38b8992e/9786"
+    },
+]
