b6f0e51186ccd230c9664a2757e6f08ab81af509
afiatamanna06 <afiatamanna06@gmail.com>
2022-11-18 19:23:06 +0600

    Add about section pencil image section

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,4 +1,4 @@
-import { Flex, Image } from "@chakra-ui/react";
+import { Box, Center, Flex, Image } from "@chakra-ui/react";
 import AboutDescription from "../components/about/AboutDescription";
 import AboutHeader from "../components/about/AboutHeader";
 
@@ -7,6 +7,20 @@ function About() {
     <Flex direction="column" w="full">
       <AboutHeader />
       <AboutDescription />
+      <Box w="full" bg="white" px="6" pb="12">
+        <Center>
+          <Flex
+            maxW="74rem"
+            gap={[5, 5, 5, 100, 100]}
+            direction={["column", "column", "column", "row", "row"]}
+            w="full"
+            alignItems={["start", "start", "center", "center", "center"]}
+          >
+            <Image display={["none", "none", "none", "flex", "flex"]} src="/pencil-drawing.png" w="70%" alt="" />
+            <Image display={["flex", "flex", "flex", "none", "none"]} transform="scaleX(-1)" src="/pencil-drawing.png" w="100%" alt="" />
+          </Flex>
+        </Center>
+      </Box>
     </Flex>
   );
 }
