5e6ded4310132c36804828896ed0ecf086b60bdf
afiatamanna06 <afiatamanna06@gmail.com>
2022-12-06 18:49:11 +0600

    Update about card file

--- components/about/AboutCards.tsx
+++ components/about/AboutCards.tsx
@@ -1,8 +1,29 @@
+import { Box, Flex, Image } from "@chakra-ui/react"
+import Link from "next/link";
 
+interface propTypes {
+  bg: string;
+  titleImg: string;
+  description: string;
+  link: string;
+  path: string;
+  image: string;
+}
 
-function AboutCards() {
+function AboutCards({bg, titleImg, description, link, path, image}: propTypes) {
   return (
-    <div>AboutCards</div>
+    <Flex direction="column" bg={bg} p="6">
+      <Flex direction="column" alignItems="start">
+        <Image src={titleImg} alt="" />
+        <Box>{description}</Box>
+        <Link href={path} passHref>
+          <Box>{`${link} >`}</Box>
+        </Link>
+      </Flex>
+      <Flex justify="center">
+        <Image src={image} alt="" />
+      </Flex>
+    </Flex>
   )
 }
 
---
+++ components/about/AboutCardsData.ts
@@ -0,0 +1,16 @@
+export const aboutCardsData = [
+    {
+        path: "https://www.forbes.com/sites/mariagraciasantillanalinares/2022/09/13/reddits-ohanian-leads-54-million-doodles-capital-raise",
+        image: "/canon.png",
+        title: "Reddit's Ohanian Leads $54 Million Doodles Capital Raise",
+        source: "Forbes",
+        date: "Sep 13, 2022",
+    },
+    {
+        path: "https://www.fastcompany.com/90783396/doodles-domination-how-a-1-year-old-nft-project-turned-into-the-next-big-thing",
+        image: "/dood-logo.png",
+        title: "Doodles Domination: How a 1-year-old NFT project turned into the next big thing",
+        source: "FastCompany",
+        date: "Sep 13, 2022",
+    },
+]
