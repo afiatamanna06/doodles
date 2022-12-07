cb96fec40f17942d5d27fd552372feee3bf6591b
afiatamanna06 <afiatamanna06@gmail.com>
2022-12-07 09:29:22 +0600

    Add nftnyc card data

---
+++ components/nftnyc/NftnycData.ts
@@ -0,0 +1,20 @@
+export const nftnycCardsData = [
+    {
+        date: "June 21 | 8:30pm",
+        name: "Launch Event",
+        location: "Palladium Times Square 1515 Broadway, New York, NY 10036",
+        events: ["Product trailer premier", "On-stage reveal ft. Burnt Toast, Poopie, Tulip, Julian Holguin", "After-party ft. special guests"]
+    },
+    {
+        date: "June 22 + 23 | 12 - 8pm",
+        name: "The Genesis Factory",
+        location: "Location to be revealed",
+        events: ["Open to the public with line by-pass for holders", "Extended holder experiences", "ShopDoodles pop-up"]
+    },
+    {
+        date: "June 22 + 23 | 2 - 10pm",
+        name: "Doodle Lounge",
+        location: "Location to be revealed",
+        events: ["Holder HQ", "Community-led events"]
+    },
+]
