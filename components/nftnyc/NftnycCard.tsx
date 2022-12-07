57155d7a2822b9f723a3c4289d25ee4ab56f7efc
afiatamanna06 <afiatamanna06@gmail.com>
2022-12-07 10:37:48 +0600

    Update nftnyc file

--- components/nftnyc/NftnycCard.tsx
+++ components/nftnyc/NftnycCard.tsx
@@ -1,8 +1,24 @@
+import { Box, Flex, ListItem, UnorderedList } from "@chakra-ui/react"
 
+interface propTypes {
+    date: string;
+    name: string;
+    location: string;
+    events: string[];
+}
 
-function NftnycCard() {
+function NftnycCard({date, name, location, events}: propTypes) {
   return (
-    <div>NftnycCard</div>
+    <Flex direction="column" flex="1">
+        <Box>{date}</Box>
+        <Box fontSize="xl">{name}</Box>
+        <Box>{location}</Box>
+        <UnorderedList>
+            {events.map((item) => (
+                <ListItem key={item}>{item}</ListItem>
+            ))}
+        </UnorderedList>
+    </Flex>
   )
 }
 
