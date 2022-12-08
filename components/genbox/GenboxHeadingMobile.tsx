7168da7b716eabc9e79c27c3ad525aea70322f83
afiatamanna06 <afiatamanna06@gmail.com>
2022-12-08 23:44:30 +0600

    Add genbox heading mobile file

---
+++ components/genbox/GenboxHeadingMobile.tsx
@@ -0,0 +1,14 @@
+import { Flex } from "@chakra-ui/react"
+
+function GenboxHeadingMobile() {
+  return (
+    <Flex
+      display={["flex", "flex", "none", "none", "none"]}
+      direction="column"
+      w="full">
+          
+    </Flex>
+  )
+}
+
+export default GenboxHeadingMobile
