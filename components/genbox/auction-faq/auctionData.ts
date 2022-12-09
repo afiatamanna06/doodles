567cf38e913f709640da0c4a5034c092f29329ec
afiatamanna06 <afiatamanna06@gmail.com>
2022-12-09 11:36:38 +0600

    Add aution data

---
+++ components/genbox/auction-faq/auctionData.ts
@@ -0,0 +1,42 @@
+export const auctionData = [
+    {
+        title: "What is Doodles 2?",
+        data: "Doodles 2 is a mass-market, identity-focused NFT that lets its collectors express themselves with wearables, animated characters, fully-licensed music and more. You can mix and match wearables on top of your Doodles 2 to suit your vibe.",
+    },
+    {
+        title: "What is in the box?",
+        data: "Wearable NFTs. A rare assortment of apparel and accessories for Doodles 2.",
+    },
+    {
+        title: "When can I use the Genesis Boxes?",
+        data: "Genesis Boxes are scheduled to be opened in 2022 with the release of Doodles 2.",
+    },
+    {
+        title: "How many Genesis Boxes are there?",
+        data: "There is a capped supply of 24,000 Genesis Boxes. Of this 24,000, up to 4,000 have been reserved by the team for distribution in its Genesis Factory IRL events for Doodlers and Dooplicator holders.",
+    },
+    {
+        title: "Why is the Genesis Box valuable?",
+        data: "Genesis Box NFTs are first edition wearables in Doodles 2. They will be used to seed the initial economy of Wearables and will be recognized within the product as limited-edition.",
+    },
+    {
+        title: "What blockchain will the Genesis Box be minted on?",
+        data: "Genesis Box will be distributed as ERC-721s on the Ethereum blockchain but may be bridged onto a new blockchain prior to them being utilized and prior Doodles 2.",
+    },
+    {
+        title: "What blockchain will the Genesis Box be usable on?",
+        data: "We're actively testing multiple blockchains for mass-market accessibility that can support Doodles 2. The final blockchain remains to be announced.",
+    },
+    {
+        title: "When will I receive my Genesis Box?",
+        data: "For successful bidder in the bucket sale, you will receive your Genesis Box once the bucket sale concludes. For Genesis Factory pre-sale purchasers, you will receive your Genesis Box once the bucket sale concludes.",
+    },
+    {
+        title: "What is a Bucket Auction?",
+        data: "Simply put, it's an auction where bidders can bid however much ETH they want in their initial bid and optionally increase their bid. The number of NFTs for sale and the auction time are both fixed. At the end of the auction, a clearing price is determined and successful bids above the clearing price will receive NFTs along with a refund of any ETH remainder from calculating individual user bid / clearing price. Unsuccessful bids below the clearing price will receive a full refund.",
+    },
+    {
+        title: "How do I win the auction?",
+        data: "To win the auction, your total bid, which includes the sum of any increased bids, must be higher than the clearing price. If your total bid is 2 and the clearing price is 0.75, you will receive two NFTs and a 0.5 ETH refund for the remainder.",
+    },
+]
