67ff62dd30664b4434ebb912762884851faf7cb1
afiatamanna06 <afiatamanna06@gmail.com>
2022-12-09 10:21:43 +0600

    Update genbox heading mobile file

--- components/genbox/GenBoxHeading.tsx
+++ components/genbox/GenBoxHeading.tsx
@@ -1,10 +1,12 @@
 import { Box, Flex } from "@chakra-ui/react";
+import GenboxHeadingMobile from "./GenboxHeadingMobile";
 import GenboxHeadingWeb from "./GenboxHeadingWeb";
 
 export default function GenBoxHeading() {
   return (
     <Flex position="relative" color="white" w="100%">
         <GenboxHeadingWeb />
+        <GenboxHeadingMobile />
     </Flex>
   )
 }
--- components/genbox/GenboxHeadingMobile.tsx
+++ components/genbox/GenboxHeadingMobile.tsx
@@ -1,4 +1,5 @@
-import { Flex, Image } from "@chakra-ui/react"
+import { Box, Button, Flex, Image } from "@chakra-ui/react";
+import Link from "next/link";
 
 function GenboxHeadingMobile() {
   return (
@@ -12,10 +13,62 @@ function GenboxHeadingMobile() {
       bgSize={["cover"]}
       position="relative"
       bgPosition="center"
-      bgRepeat="no-repeat">
-        <Image src="/genbox.gif" w="100%" h="100%" alt="" />  
+      bgRepeat="no-repeat"
+    >
+      <Image
+        src="/genbox.gif"
+        w={["100%", "100%", "70%"]}
+        h={["60%", "60%", "75%"]}
+        top={["40%", "40%", "27%"]}
+        position="absolute"
+        zIndex={0}
+        alt=""
+      />
+      <Flex
+        direction="column"
+        alignItems="center"
+        justify="space-between"
+        minH={["80vh", "80vh", "65vh"]}
+        py="12"
+        zIndex={1}
+      >
+        <Flex direction="column" gap={6} alignItems="center">
+          <Image src="/genbox_logo.png" w={["75%", "75%", "40%"]} alt="" />
+
+          <Flex
+            gap={[3, 3, 12, 12, 12]}
+            fontSize="lg"
+            direction={["column", "row"]}
+            alignItems={["center", "center"]}
+          >
+            <Box>TOTAL SUPPLY: 24,000</Box>
+            <Box>PRICE: Mkt</Box>
+          </Flex>
+        </Flex>
+        <Flex w="full" justify="center">
+          <Link
+            href="https://opensea.io/collection/genesis-box"
+            target="_blank"
+            passHref
+          >
+            <Button
+              borderColor="#a4a4f4"
+              px={12}
+              borderBottomWidth="2px"
+              color="#8a7dea"
+              py={6}
+              borderRadius="xl"
+              _hover={{ opacity: ".85" }}
+              _active={{ opacity: ".85" }}
+              _focus={{ opacity: ".85" }}
+            >
+              <Box fontSize="xl">buy on opensea</Box>
+            </Button>
+          </Link>
+        </Flex>
+      </Flex>
     </Flex>
-  )
+  );
 }
 
-export default GenboxHeadingMobile
+export default GenboxHeadingMobile;
