2aa404680d2198cf6ae3fbfbcfda02789c1e5911
afiatamanna06 <afiatamanna06@gmail.com>
2022-12-09 20:30:17 +0600

    Add auction section connect

--- components/genbox/auction-faq/AuctionSection.tsx
+++ components/genbox/auction-faq/AuctionSection.tsx
@@ -1,5 +1,16 @@
-import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box, Flex, Image } from "@chakra-ui/react";
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+  Button,
+  Flex,
+  Image,
+} from "@chakra-ui/react";
 import { auctionData } from "./auctionData";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 function AuctionSection() {
   return (
@@ -9,16 +20,25 @@ function AuctionSection() {
           direction="column"
           maxW="50rem"
           pt={["14", "28", "28"]}
-          pb="12"
           w="full"
           color="black"
           px={[4, 4, 4, 4, 0]}
           alignItems={["start"]}
         >
-          <Image src="/FAQ_header.png" mb="6" w={["95%", "95%", "55%", "35%"]} alt="" />
-          <Accordion defaultIndex={[0]} allowMultiple width="full">
+          <Image
+            src="/FAQ_header.png"
+            mb="6"
+            w={["95%", "95%", "55%", "35%"]}
+            alt=""
+          />
+          <Accordion defaultIndex={[0]} allowMultiple width="full" pb={["24", "24", "24", "28"]}>
             {auctionData.map(({ title, data }) => (
-              <AccordionItem key={title} background="#fafafa" mb="2" borderRadius="lg">
+              <AccordionItem
+                key={title}
+                background="#fafafa"
+                mb="2"
+                borderRadius="lg"
+              >
                 <h2>
                   <AccordionButton>
                     <Box flex="1" textAlign="left" fontWeight="semibold">
@@ -33,6 +53,38 @@ function AuctionSection() {
               </AccordionItem>
             ))}
           </Accordion>
+          <Flex
+            gap={[4]}
+            direction={["column", "column", "column", "row"]}
+            alignItems={["start"]}
+            w="full"
+          >
+            <Flex
+              direction="column"
+              p="3"
+              w="full"
+              position="relative"
+              color="white"
+              bg="rgb(154 199 250)"
+              borderRadius="xl"
+              alignItems="center"
+            >
+              <Image src="/auction-box-header.png" w={["60%", "60%", "50%", "40%"]} position="absolute" top={[-8, -8, -12, -14]} left={["20%", "20%", "25%", "30%"]} zIndex={1} alt="" />
+              <Box fontSize="xl">the auction has ended</Box>
+              <Box fontSize="sm" mb="4">no new bids will be accepted</Box>
+              <Button
+                as={ConnectButton}
+                label="connect"
+                bg="#5086DC"
+                size="sm"
+                borderRadius="lg"
+                px="4"
+                h="1.5rem"
+                color="white"
+                _hover={{ bg: "#99E2FF", boxShadow: "dark-lg" }}
+              ></Button>
+            </Flex>
+          </Flex>
         </Flex>
       </Flex>
     </Box>
