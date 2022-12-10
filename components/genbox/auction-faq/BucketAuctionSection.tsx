c4349556ad7a10fcd1d8ee7b78cf142e8942a66e
afiatamanna06 <afiatamanna06@gmail.com>
2022-12-10 19:40:06 +0600

    Add bucket data file

--- components/genbox/auction-faq/BucketAuctionSection.tsx
+++ components/genbox/auction-faq/BucketAuctionSection.tsx
@@ -1,4 +1,5 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
+import { FaEthereum } from "react-icons/fa"
 
 function BucketAuctionSection() {
   return (
@@ -8,6 +9,16 @@ function BucketAuctionSection() {
       position="relative"
       alignItems="center"
     >
+        <Flex bg="rgb(140 125 234)" w="full" py="1" px="4" borderRadius="lg" fontSize="sm" color="white">
+            <Box w="10%">Rank</Box>
+            <Flex w="15%" gap={1} alignItems="center">
+              <FaEthereum />
+              <Box>Bid</Box>
+            </Flex>
+            <Box w="45%">Bidder</Box>
+            <Box w="15%">Boxes</Box>
+            <Box w="15%">Refund</Box>
+        </Flex>
     </Flex>
   );
 }
