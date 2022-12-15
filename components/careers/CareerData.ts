d1d93fd6d317ed3522320a70fb77514a8c2d13b7
afiatamanna06 <afiatamanna06@gmail.com>
2022-12-15 22:16:00 +0600

    Add career data

---
+++ components/careers/CareerData.ts
@@ -0,0 +1,77 @@
+export const careerData = [
+    {
+        title: "Finance & Operations",
+        links: [
+            {
+                path: "https://jobs.ashbyhq.com/Doodles/491c152e-d56f-4b92-bfe2-0e80f06d92b1",
+                name: "Executive Assistant",
+                place: "Miami"
+            },
+            {
+                path: "https://jobs.ashbyhq.com/Doodles/bb6a9e79-8c9f-4266-8783-ee817415f526",
+                name: "Head of Strategic Finance",
+                place: "Miami"
+            },
+        ]
+    },
+    {
+        title: "Creative",
+        links: [
+            {
+                path: "https://jobs.ashbyhq.com/Doodles/ed8c73eb-2ec4-47df-9219-406570cdf7a3",
+                name: "Graphic Designer",
+                place: "Miami"
+            },
+            {
+                path: "https://jobs.ashbyhq.com/Doodles/38b71199-03db-4da7-82fe-50b49fafb45e",
+                name: "Illustrator",
+                place: "Miami"
+            },
+        ]
+    },
+    {
+        title: "Legal",
+        links: [
+            {
+                path: "https://jobs.ashbyhq.com/Doodles/77f61ddb-804b-457a-ada8-a44cfa23c41d",
+                name: "Head of Business Affairs",
+                place: "Miami"
+            },
+        ]
+    },
+    {
+        title: "Marketing",
+        links: [
+            {
+                path: "https://jobs.ashbyhq.com/Doodles/a718184a-adac-45f9-adfa-d206119dee30",
+                name: "Head of Marketing",
+                place: "Miami"
+            },
+        ]
+    },
+    {
+        title: "Product",
+        links: [
+            {
+                path: "https://jobs.ashbyhq.com/Doodles/ce9e6fca-b194-4ea8-8b9d-b22370c7e0fd",
+                name: "Product Manager",
+                place: "Remote"
+            },
+            {
+                path: "https://jobs.ashbyhq.com/Doodles/5bde1d0d-0ce6-4fa1-8d2c-f99b0d3e8328",
+                name: "UX/UI Designer",
+                place: "Remote"
+            },
+        ]
+    },
+    {
+        title: "Engineering",
+        links: [
+            {
+                path: "https://jobs.ashbyhq.com/Doodles/aca33104-9673-40cf-8c33-1f8f1a499e16",
+                name: "Senior Front End Engineer",
+                place: "Remote"
+            },
+        ]
+    },
+]
