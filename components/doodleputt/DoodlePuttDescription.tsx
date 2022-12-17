ed4d6c8098730bf5c0f14761adb00e8910a885dd
afiatamanna06 <afiatamanna06@gmail.com>
2022-12-17 11:14:36 +0600

    Add doodle putt heading images

--- components/doodleputt/DoodlePuttDescription.tsx
+++ components/doodleputt/DoodlePuttDescription.tsx
@@ -1,9 +1,12 @@
-import { Flex } from "@chakra-ui/react"
+import { Flex, Image } from "@chakra-ui/react"
 
 function DoodlePuttDescription() {
   return (
-    <Flex direction="column">
-
+    <Flex direction="column" position="relative" alignItems="center" bg="#99e2ff">
+        <Image src="/doodleputt_cover.png" objectFit="cover" h={["30rem", "30rem", "30rem", "full"]} display={["none", "none", "flex", "flex", "flex"]} alt="" />
+        <Image src="/doodleputt_cover_mobile.png" display={["flex", "flex", "none", "none", "none"]} alt="" />
+        <Image src="/doodleputt_logo.png" w="25%" h="25%" mt="16" display={["none", "none", "none", "flex", "flex"]} alt="" />
+        <Image src="/doodleputt_logo.png" w={["80%", "80%", "40%"]} position="absolute" display={["flex", "flex", "flex", "none", "none"]} alt="" />
     </Flex>
   )
 }
