8e8b4d8f2bd1d5e50d53e427c88f534993c17f89
afiatamanna06 <afiatamanna06@gmail.com>
2022-12-18 13:10:24 +0600

    Add doodle putt data file

---
+++ components/doodleputt/DoodlePuttFacilitiesData.ts
@@ -0,0 +1,17 @@
+export const facilitiesData = [
+    {
+        image: "/putt_opensea_lounge.svg",
+        title: "Opensea Lounge",
+        description: "Enjoy 10,000 sqft of outdoor amenities, food, beverages, and entertainment while you wait for your tee time or relax after completing a round with your friends.",
+    },
+    {
+        image: "/putt_shop.svg",
+        title: "Pro Shop",
+        description: "Shop our brand-new collection of official products from shopDoodles including apparel, accessories, and golf gear.",
+    },
+    {
+        image: "/putt_vip.svg",
+        title: "VIP Doodle Holders",
+        description: "If you have a Doodle, you'll receive priority tee-time registration, complimentary access with open bar, access to an onsite VIP holder lounge, and a customization station.",
+    },
+]
