269eaf81357bd2eb4f35393cae49c3f968cd7e08
afiatamanna06 <afiatamanna06@gmail.com>
2022-12-18 12:38:15 +0600

    Update doodle putt facilities file

--- components/doodleputt/DoodlePuttFacilities.tsx
+++ components/doodleputt/DoodlePuttFacilities.tsx
@@ -1,9 +1,34 @@
-
+import { Box, Center, Flex } from "@chakra-ui/react";
 
 function DoodlePuttFacilities() {
   return (
-    <div>DoodlePuttFacilities</div>
-  )
+    <Box
+      w="full"
+      bgImage="url('/putt_bottom_bg.png')"
+      bgSize={["cover"]}
+      bgPosition="right"
+      position="relative"
+      borderRadius="2xl"
+      bgRepeat="no-repeat"
+      px="4"
+      py="6"
+    >
+      <Center>
+        <Flex
+          maxW="74rem"
+          gap={5}
+          direction="column"
+          color="rgb(173, 153, 153)"
+          w="full"
+          alignItems={["center"]}
+        >
+            <Flex direction={["column", "column", "column", "row", "row"]}>
+
+            </Flex>
+        </Flex>
+      </Center>
+    </Box>
+  );
 }
 
-export default DoodlePuttFacilities
+export default DoodlePuttFacilities;
