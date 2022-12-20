d5e5807eb713d8be1b2fcd7ba3febaa5c2ddb35e
afiatamanna06 <afiatamanna06@gmail.com>
2022-12-20 11:14:57 +0600

    Add putt resources data

---
+++ components/doodleputt/PuttResourcesData.ts
@@ -0,0 +1,38 @@
+export const resourcesData = [
+    {
+        title: "How do I attend?",
+        data: "DoodlePutt early registration has ended. Doodle holders can still attempt to register on site, but capacity is very limited and mini-golf tee times are not guaranteed.General public tickets may be purchased on site subject to availability and capacity. All general public reservations and tickets must be purchased on-site during event hours.",
+    },
+    {
+        title: "How do I register as a Doodle holder?",
+        data: "Early registration for Doodle holders has ended. Doodle holders can still attempt to register on site, but capacity is very limited and mini-golf tee times are not guaranteed. We will not be able to accommodate late registrations. If you have questions about existing tickets that were successfully reserved, please reach out to support staff in our dedicated tokenproof channel in our Discord: https://discord.com/invite/doodles",
+    },
+    {
+        title: "How do I register my +1 guest?",
+        data: "During the tokenproof registration process, you will be asked if you are bringing a guest. That person can be anyone - as long as they are in line with you on-site at the event when you check in to receive your badge. They will receive their own badge at that time.",
+    },
+    {
+        title: "Can I attend if I own a Dooplicator/Genesis Box?",
+        data: "Pre-registration and free entry has been limited to Doodle holders only. Others are welcome to arrive on-site to obtain a general admission ticket, but there is no guarantee for entry as capacity is limited.",
+    },
+    {
+        title: "Can I transfer my ticket?",
+        data: "No. All pre-registrations for Doodle holders are non-transferable.",
+    },
+    {
+        title: "How do I cancel my ticket?",
+        data: "Information on ticket cancellations will be made available close to the event date. We encourage all pre-registered Doodle-holders who no longer plan on attending to cancel their ticket to allow others to enjoy the experience.",
+    },
+    {
+        title: "Is the event all ages?",
+        data: "Yes, all ages are encouraged! Access to the bars and some areas may be limited to 21 and over with valid ID.",
+    },
+    {
+        title: "Where is the event?",
+        data: "The event is located at 2534 N Miami Ave, Miami. You can find directions here.",
+    },
+    {
+        title: "Where can I park?",
+        data: "Several parking lots are located within close walking distance to the event. You can find a list of nearby Wynwood-area parking lots here.",
+    },
+]
