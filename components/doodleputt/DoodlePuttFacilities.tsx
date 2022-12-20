b48f1ffcab437f67803b573bb895d7da05b1247c
afiatamanna06 <afiatamanna06@gmail.com>
2022-12-20 10:34:01 +0600

    Add livestream section

--- components/doodleputt/DoodlePuttFacilities.tsx
+++ components/doodleputt/DoodlePuttFacilities.tsx
@@ -1,4 +1,5 @@
-import { Box, Center, Flex } from "@chakra-ui/react";
+import { Box, Center, Flex, Image } from "@chakra-ui/react";
+import Link from "next/link";
 import DoodlePuttFacilitiesCard from "./DoodlePuttFacilitiesCard";
 import { facilitiesData } from "./DoodlePuttFacilitiesData";
 
@@ -16,7 +17,7 @@ function DoodlePuttFacilities() {
       <Center>
         <Flex
           maxW="65rem"
-          gap={5}
+          gap={12}
           direction="column"
           color="rgb(173, 153, 153)"
           w="full"
@@ -27,6 +28,26 @@ function DoodlePuttFacilities() {
                 {facilitiesData.map(({image, title, description}) => (
                     <DoodlePuttFacilitiesCard key={title} image={image} title={title} description={description} />
                 ))}
+            </Flex>
+            <Flex direction={["column", "column", "row", "row", "row"]} justify="space-between" alignItems="center" gap={[6, 6, 6, 14]}>
+                <Flex w="45%" display={["flex", "flex", "none", "none", "none"]}>
+                  <Link href="https://mobile.twitter.com/juicykatbae" passHref>
+                    <Image src="/juicykatbae.png" w="100%" alt="" />
+                  </Link>
+                </Flex>
+                <Box>
+                  <Box fontWeight="semibold" textAlign={["center", "center", "start", "start"]}>On-location livestream hosted by JuicyKatBae</Box>
+                  <Box textAlign={["center", "center", "start", "start"]}>
+                    JuicyKatBae is comprised of three OG Doodle holders who met on the Blockchain: Julia, Kat Sammy. Despite their geographical distance apart, the trio is united through their love of the Doodles community and fascination with all things Web3.
+                  </Box>
+                  <br />
+                  <Box textAlign={["center", "center", "start", "start"]}>&quot;JKB&quot; hosts weekly Twitter Spaces, “JuicyKatBae Talk About Nothing” on Tuesdays at 8pm ET.</Box>
+                </Box>
+                <Flex display={["none", "none", "flex", "flex", "flex"]}>
+                  <Link href="https://mobile.twitter.com/juicykatbae" passHref>
+                    <Image src="/juicykatbae.png" w="100%" alt="" />
+                  </Link>
+                </Flex>
             </Flex>
         </Flex>
       </Center>
