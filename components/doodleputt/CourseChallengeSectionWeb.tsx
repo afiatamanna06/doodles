e20ccf878d485d61dd407266ae2f24ca1d34c958
afiatamanna06 <afiatamanna06@gmail.com>
2022-12-24 02:52:56 +0600

    Add golf ball animation

--- components/doodleputt/CourseChallengeSectionWeb.tsx
+++ components/doodleputt/CourseChallengeSectionWeb.tsx
@@ -1,13 +1,37 @@
-import { Box, Flex, Image } from "@chakra-ui/react"
+import { Box, Flex, Image } from "@chakra-ui/react";
+import { Parallax } from "react-scroll-parallax";
 
 function CourseChallengeSectionWeb() {
   return (
-    <Flex direction="column" color="white" mt={["10", "10", "10", "20"]} gap={5} display={["none", "none", "none", "flex", "flex"]}>
-      <Image src="/text_challenge-awaits.png" w="30%" alt="" />
-      <Image src="/text_sink-a-hole.png" w="35%" alt="" />
-      <Box>*Subject to availability</Box>
+    <Flex
+      color="white"
+      w="100%"
+      gap={20}
+      mt={["10", "10", "10", "20"]}
+      display={["none", "none", "none", "flex", "flex"]}
+    >
+      <Flex direction="column" gap={5} w="30%">
+        <Image src="/text_challenge-awaits.png" alt="" />
+        <Image src="/text_sink-a-hole.png" alt="" />
+        <Box>*Subject to availability</Box>
+      </Flex>
+      <Flex direction="column" alignItems="center" w="15%">
+        <Image src="/stars_sparkle.png" mb="-8" zIndex={2} alt="" />
+        <Image src="/wormhole.png" alt="" />
+      </Flex>
+      <Parallax
+        speed={14}
+        rotate={[
+          '70deg',
+          '0deg'
+        ]}
+        translateX={["50%", "-50%"]}
+        translateY={["38%", "38%"]}
+      >
+        <Image src="/golf_ball.png" w="50%" alt="" />
+      </Parallax>
     </Flex>
-  )
+  );
 }
 
-export default CourseChallengeSectionWeb
+export default CourseChallengeSectionWeb;
