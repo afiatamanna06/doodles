e4fc26279128bfc05a22dcd36131b13c7cfb4d7a
afiatamanna06 <afiatamanna06@gmail.com>
2022-12-23 14:14:14 +0600

    Update course spacings

--- components/doodleputt/CourseCard.tsx
+++ components/doodleputt/CourseCard.tsx
@@ -9,7 +9,7 @@ interface propTypes {
 function CourseCard({ image, id, title }: propTypes) {
   return (
     <Flex direction="column" alignItems="center" color="#6dd1a1" bg="#ffffff">
-        <Image src={image} objectFit="cover" h="12rem" w="full" alt="" />
+        <Image src={image} objectFit="cover" h="14rem" w="full" alt="" />
         <Box fontSize="lg">{id}</Box>
         <Box fontSize="sm" pb="2" textAlign="center">{title}</Box>
     </Flex>
