0503bb81673821cc9cad73109ae20fbff40325b2
afiatamanna06 <afiatamanna06@gmail.com>
2022-12-23 13:28:11 +0600

    Add course card file

---
+++ components/doodleputt/CourseCard.tsx
@@ -0,0 +1,11 @@
+import { Flex } from "@chakra-ui/react"
+
+function CourseCard() {
+  return (
+    <Flex direction="column" alignItems="center" color="#6dd1a1" bg="#ffffff">
+
+    </Flex>
+  )
+}
+
+export default CourseCard
