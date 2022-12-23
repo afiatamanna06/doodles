7ce3e664e2c4ce8d7c4bffd71fa9ab180671ae95
afiatamanna06 <afiatamanna06@gmail.com>
2022-12-23 13:11:07 +0600

    Add course card data

---
+++ components/doodleputt/CourseCardData.ts
@@ -0,0 +1,38 @@
+export const courseCardData = [
+    {
+        image: "/hole_1.png",
+        title: "Basement",
+    },
+    {
+        image: "/hole_2.png",
+        title: "Dirty World",
+    },
+    {
+        image: "/hole_3.png",
+        title: "Doodle World",
+    },
+    {
+        image: "/hole_4.png",
+        title: "Space",
+    },
+    {
+        image: "/hole_5.png",
+        title: "Tropics",
+    },
+    {
+        image: "/hole_6.png",
+        title: "Desert",
+    },
+    {
+        image: "/hole_7.png",
+        title: "Portal Cave",
+    },
+    {
+        image: "/hole_8.png",
+        title: "Bubblegum",
+    },
+    {
+        image: "/hole_9.png",
+        title: "Rainbow Clouds",
+    },
+]
