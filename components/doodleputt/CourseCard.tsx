e24374c2ee08e35945360819ade644035566679d
afiatamanna06 <afiatamanna06@gmail.com>
2022-12-23 13:34:30 +0600

    Update course card file

--- components/doodleputt/CourseCard.tsx
+++ components/doodleputt/CourseCard.tsx
@@ -1,9 +1,17 @@
-import { Flex } from "@chakra-ui/react"
+import { Box, Flex, Image } from "@chakra-ui/react"
 
-function CourseCard() {
+interface propTypes {
+    image: string;
+    id: number;
+    title: string;
+}
+
+function CourseCard({ image, id, title }: propTypes) {
   return (
     <Flex direction="column" alignItems="center" color="#6dd1a1" bg="#ffffff">
-
+        <Image src={image} alt="" />
+        <Box fontSize="lg">{id}</Box>
+        <Box fontSize="sm">{title}</Box>
     </Flex>
   )
 }
