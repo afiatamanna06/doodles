43e7298759c34f667bf494cab56de16a907f5d89
afiatamanna06 <afiatamanna06@gmail.com>
2022-12-25 22:34:53 +0600

    Fix doodle putt facilities card file

--- components/doodleputt/DoodlePuttFacilitiesCard.tsx
+++ components/doodleputt/DoodlePuttFacilitiesCard.tsx
@@ -1,19 +1,27 @@
 import { Box, Flex, Image } from "@chakra-ui/react";
 
 interface propTypes {
-    image: string;
-    title: string;
-    description: string
+  image: string;
+  title: string;
+  description: string;
 }
 
 function DoodlePuttFacilitiesCard({ image, title, description }: propTypes) {
   return (
-    <Flex direction="column" gap={1} alignItems={["center", "center", "center", "start"]}>
-        <Image src={image} h="3rem" w="3rem" alt="" />
-        <Box fontWeight="semibold" mt="1">{title}</Box>
-        <Box textAlign={["center", "center", "center", "start"]}>{description}</Box>
+    <Flex
+      direction="column"
+      gap={1}
+      alignItems={["center", "center", "center", "start"]}
+    >
+      <Image src={image} h="3rem" w="3rem" alt="" />
+      <Box fontWeight="semibold" mt="1">
+        {title}
+      </Box>
+      <Box textAlign={["center", "center", "center", "start"]}>
+        {description}
+      </Box>
     </Flex>
-  )
+  );
 }
 
-export default DoodlePuttFacilitiesCard
+export default DoodlePuttFacilitiesCard;
