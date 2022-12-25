ae2f075cb1512c44ff1f06b700c7e453543c658d
afiatamanna06 <afiatamanna06@gmail.com>
2022-12-25 22:35:26 +0600

    Fix doodle putt cloud marquee file

--- components/doodleputt/PuttCloudMarquee.tsx
+++ components/doodleputt/PuttCloudMarquee.tsx
@@ -3,24 +3,53 @@ import Marquee from "react-fast-marquee";
 
 function PuttCloudMarquee() {
   return (
-    <Marquee
-      delay={12}
-      direction="right"
-      play={true}
-      gradientWidth={0}
-    >
+    <Marquee delay={12} direction="right" play={true} gradientWidth={0}>
       <Flex mx={40} gap={[4, 10]} direction="column">
-        <Image src="/cloud1.png" w={["100%", "100%", "100%", "100%"]} h={["1rem", "1rem", "3rem", "3rem"]} alt="" />
-        <Image src="/cloud1.png" ml={["-8", "-8", "-20", "-20"]} w={["80%", "80%", "60%", "60%"]} h={[".7rem", ".7rem", "2rem", "2rem"]} alt="" />
+        <Image
+          src="/cloud1.png"
+          w={["100%", "100%", "100%", "100%"]}
+          h={["1rem", "1rem", "3rem", "3rem"]}
+          alt=""
+        />
+        <Image
+          src="/cloud1.png"
+          ml={["-8", "-8", "-20", "-20"]}
+          w={["80%", "80%", "60%", "60%"]}
+          h={[".7rem", ".7rem", "2rem", "2rem"]}
+          alt=""
+        />
       </Flex>
-      <Image src="/cloud1.png" mx={80} w={["10%", "10%", "10%", "10%"]} h={["2rem", "2rem", "5rem", "5rem"]} alt="" />
+      <Image
+        src="/cloud1.png"
+        mx={80}
+        w={["10%", "10%", "10%", "10%"]}
+        h={["2rem", "2rem", "5rem", "5rem"]}
+        alt=""
+      />
       <Flex mx={40} gap={[4, 10]} direction="column">
-        <Image src="/cloud1.png" w={["100%", "100%", "100%", "100%"]} h={["1rem", "1rem", "3rem", "3rem"]} alt="" />
-        <Image src="/cloud1.png" ml={["-8", "-8", "-20", "-20"]} w={["80%", "80%", "60%", "60%"]} h={[".7rem", ".7rem", "2rem", "2rem"]} alt="" />
+        <Image
+          src="/cloud1.png"
+          w={["100%", "100%", "100%", "100%"]}
+          h={["1rem", "1rem", "3rem", "3rem"]}
+          alt=""
+        />
+        <Image
+          src="/cloud1.png"
+          ml={["-8", "-8", "-20", "-20"]}
+          w={["80%", "80%", "60%", "60%"]}
+          h={[".7rem", ".7rem", "2rem", "2rem"]}
+          alt=""
+        />
       </Flex>
-      <Image src="/cloud1.png" mx={80} w={["10%", "10%", "10%", "10%"]} h={["2rem", "2rem", "5rem", "5rem"]} alt="" />
+      <Image
+        src="/cloud1.png"
+        mx={80}
+        w={["10%", "10%", "10%", "10%"]}
+        h={["2rem", "2rem", "5rem", "5rem"]}
+        alt=""
+      />
     </Marquee>
-  )
+  );
 }
 
-export default PuttCloudMarquee
+export default PuttCloudMarquee;
