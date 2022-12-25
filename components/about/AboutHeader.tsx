e6863bf40efe223bf3963140a48f18bd8b6987de
afiatamanna06 <afiatamanna06@gmail.com>
2022-12-25 22:39:35 +0600

    Fix about header file

--- components/about/AboutHeader.tsx
+++ components/about/AboutHeader.tsx
@@ -1,17 +1,25 @@
-import { Flex, Image } from '@chakra-ui/react'
-import React from 'react'
-import AboutMarquee from './AboutMarquee'
+import { Flex, Image } from "@chakra-ui/react";
+import React from "react";
+import AboutMarquee from "./AboutMarquee";
 
 function AboutHeader() {
   return (
     <Flex direction="column" position="relative" pt="1" gap={1}>
-      <AboutMarquee direction='right' />
+      <AboutMarquee direction="right" />
       <AboutMarquee direction="left" />
-      <AboutMarquee direction='right' />
+      <AboutMarquee direction="right" />
       <AboutMarquee direction="left" />
-      <Image position="absolute" zIndex={2} top={["43%", "43%", "43%", "44%", "41%"]} left={["15%", "15%", "20%", "40%", "37%"]} w={["70%", "70%", "60%", "25%"]} src="/logo_outline.png" alt="" />
+      <Image
+        position="absolute"
+        zIndex={2}
+        top={["43%", "43%", "43%", "44%", "41%"]}
+        left={["15%", "15%", "20%", "40%", "37%"]}
+        w={["70%", "70%", "60%", "25%"]}
+        src="/logo_outline.png"
+        alt=""
+      />
     </Flex>
-  )
+  );
 }
 
-export default AboutHeader
+export default AboutHeader;
