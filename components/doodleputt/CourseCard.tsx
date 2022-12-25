16d2f7e65dbc957edf38c344195db8d8d508cc41
afiatamanna06 <afiatamanna06@gmail.com>
2022-12-25 22:32:05 +0600

    Fix border radius issue in doodle putt course cards

--- components/doodleputt/CourseCard.tsx
+++ components/doodleputt/CourseCard.tsx
@@ -1,19 +1,50 @@
-import { Box, Flex, Image } from "@chakra-ui/react"
+import { Box, Flex, Image } from "@chakra-ui/react";
 
 interface propTypes {
-    image: string;
-    id: number;
-    title: string;
+  image: string;
+  id: number;
+  title: string;
 }
 
 function CourseCard({ image, id, title }: propTypes) {
   return (
-    <Flex direction="column" alignItems="center" color="#6dd1a1" bg="#ffffff">
-        <Image src={image} objectFit="cover" h="14rem" w="full" alt="" />
-        <Box fontSize="lg">{id}</Box>
-        <Box fontSize="sm" pb="2" textAlign="center">{title}</Box>
+    <Flex
+      direction="column"
+      borderTopLeftRadius={id === 1 ? "lg" : "none"}
+      borderBottomLeftRadius={
+        (id === 1 && ["", "", "lg", "lg"]) ||
+        (id === 7 && ["lg", "lg", "", ""]) ||
+        "none"
+      }
+      borderTopRightRadius={
+        (id === 9 && ["", "", "lg", "lg"]) ||
+        (id === 3 && ["lg", "lg", "", ""]) ||
+        "none"
+      }
+      borderBottomRightRadius={id === 9 ? "lg" : "none"}
+      alignItems="center"
+      color="#6dd1a1"
+      bg="#ffffff"
+    >
+      <Image
+        src={image}
+        borderTopLeftRadius={id === 1 ? "lg" : "none"}
+        borderTopRightRadius={
+          (id === 9 && ["", "", "lg", "lg"]) ||
+          (id === 3 && ["lg", "lg", "", ""]) ||
+          "none"
+        }
+        objectFit="cover"
+        h="14rem"
+        w="full"
+        alt=""
+      />
+      <Box fontSize="lg">{id}</Box>
+      <Box fontSize="sm" pb="2" textAlign="center">
+        {title}
+      </Box>
     </Flex>
-  )
+  );
 }
 
-export default CourseCard
+export default CourseCard;
